f5495fbc9c6c0e513d165da3d58cd0c3f5b236d6
那須裕介 <nasuyusuke@MacBook-Pro.local>
2023-03-12 21:48:27 +0900

    ログイン後、アカウント設定からユーザ詳細画面の表示を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'rooms/index' ,to: 'rooms#index'
+  get 'users/show' ,to:'users#show'
+  resources :rooms
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
